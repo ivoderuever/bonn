6c256b0c5a88174ca7c653cab3bf9175d8f89064
ivoderuever <ivoderuever@gmail.com>
2023-12-01 16:17:53 +0100

    Update python code printer

--- print.py
+++ print.py
@@ -1,11 +1,11 @@
 import sys
 from escpos import *
 
-print(sys.argv[1])
+print('Printing:' + sys.argv[1])
 
-# Printer code for raspy
-# Epson = printer.Usb(0x04b8,0x0202)
-# Epson.text('Hello World')
-# Epson.cut()
+#Printer code for raspy
+Epson = printer.Usb(0x04b8,0x0e28)
+Epson.text(sys.argv[1])
+Epson.cut()
 
 sys.stdout.flush()
