3714f3c7ff47fe3692e0a7fa20ae842549f05ce4
ivoderuever <ivoderuever@gmail.com>
2023-12-01 16:38:34 +0100

    Add ip py

--- print.py
+++ print.py
@@ -1,11 +1,24 @@
 import sys
 from escpos import *
+import socket
 
-print('Printing:' + sys.argv[1])
+def get_ip():
+  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  s.settimeout(0)
+  # Doesn’t even have to be reachable.
+  s.connect_ex(("10.254.254.254", 1))
+  ip = s.getsockname()[0]
+  ip = "127.0.0.1" if ip == "0.0.0.0" else ip
+  s.close()
+  return ip
+
+IP = get_ip()
+print(IP + ":8080")
 
 #Printer code for raspy
 Epson = printer.Usb(0x04b8,0x0e28)
 Epson.text(sys.argv[1])
 Epson.cut()
 
+print('Printing:' + sys.argv[1])
 sys.stdout.flush()
