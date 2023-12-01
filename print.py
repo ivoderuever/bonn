efc9d910e4fea86cc8e408dfb75029ab4445703c
ivoderuever <ivoderuever@gmail.com>
2023-12-01 19:16:20 +0100

    Fix alignment and syntax error in print.py

--- print.py
+++ print.py
@@ -9,8 +9,8 @@ Epson.text("What do you fear for the earth's future? \n\n\n\n")
 Epson.set(align="left", font="a", width=3, height=3)
 Epson.text(sys.argv[1])
 
-Epson.set(align="center", width=1, height=2, font="a",)
-Epson.image("whale.png", center=True impl="bitImageColumn")
+Epson.set(align="center", width=1, height=2, font="a")
+Epson.image("whale.png", center=True, impl="bitImageColumn")
 # Epson.qr('https://deruever.nl/', ec=0, size=9, model=2, native=True)
 Epson.text("\n Write with us \n\n\n\n")
 
