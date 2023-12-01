f34268ba91059a969e6f8ceb12d0b9b7236fd1ef
ivoderuever <ivoderuever@gmail.com>
2023-12-01 18:55:04 +0100

    Add image to recipt

--- print.py
+++ print.py
@@ -3,8 +3,17 @@ from escpos import *
 
 #Printer code for raspy
 Epson = printer.Usb(0x04b8,0x0e28)
+Epson.set(align="center", font="a", width=2, height=3)
+Epson.text("What do you fear for the earth's future? \n\n\n\n")
+
+Epson.set(align="center", font="a", width=2, height=2)
 Epson.text(sys.argv[1])
-Epson.qr('https://deruever.nl/', ec=0, size=3, model=2, native=False, center=True, impl='bitImageRaster')
+
+Epson.set(align="center", width=1, height=2, font="a",)
+Epson.image("whale.png")
+# Epson.qr('https://deruever.nl/', ec=0, size=9, model=2, native=True)
+Epson.text("\n Write with us \n\n\n\n")
+
 Epson.cut()
 
 print('Printing:' + sys.argv[1])
