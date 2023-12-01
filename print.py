d9bd7cd498a5675022c41314a1cc895f4665136b
ivoderuever <ivoderuever@gmail.com>
2023-12-01 19:15:42 +0100

    Update printer settings and image alignment

--- print.py
+++ print.py
@@ -3,14 +3,14 @@ from escpos import *
 
 #Printer code for raspy
 Epson = printer.Usb(0x04b8,0x0e28)
-Epson.set(align="center", font="a", width=2, height=3)
+Epson.set(align="center", font="a", width=1, height=2)
 Epson.text("What do you fear for the earth's future? \n\n\n\n")
 
-Epson.set(align="left", font="a", width=2, height=2)
+Epson.set(align="left", font="a", width=3, height=3)
 Epson.text(sys.argv[1])
 
 Epson.set(align="center", width=1, height=2, font="a",)
-Epson.image("whale.png", impl="bitImageColumn")
+Epson.image("whale.png", center=True impl="bitImageColumn")
 # Epson.qr('https://deruever.nl/', ec=0, size=9, model=2, native=True)
 Epson.text("\n Write with us \n\n\n\n")
 
