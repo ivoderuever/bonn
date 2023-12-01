c5bf6f2380cec5192a48f39427ef578cf968fb0a
ivoderuever <ivoderuever@gmail.com>
2023-12-01 19:29:30 +0100

    Fix formatting and add newline at the end of the file

--- print.py
+++ print.py
@@ -7,14 +7,14 @@ Epson.set(align="center", font="a", width=2, height=3)
 Epson.text("What do you fear for the earth's future? \n\n\n\n")
 
 Epson.set(align="left", font="a", width=3, height=3)
-Epson.text(sys.argv[1])
+Epson.text(sys.argv[1] + "\n\n")
 
 Epson.set(align="center", width=1, height=2, font="a")
 Epson.image("qr.jpg", impl="bitImageColumn")
 # Epson.qr('https://deruever.nl/', ec=0, size=9, model=2, native=True)
-Epson.text("\n Write with us \n\n\n\n")
+#Epson.text("\n Write with us \n\n\n\n")
 
-# Epson.cut()
+Epson.cut()
 
 print('Printing:' + sys.argv[1])
 sys.stdout.flush()
