3bc78cb80ef34bf2bda0caed1c6252572f4e08af
ivoderuever <ivoderuever@gmail.com>
2023-12-01 17:58:15 +0100

    Add qr code test 2

--- print.py
+++ print.py
@@ -1,23 +1,10 @@
 import sys
 from escpos import *
-import socket
-
-def get_ip():
-  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  s.settimeout(0)
-  # Doesn’t even have to be reachable.
-  s.connect_ex(("10.254.254.254", 1))
-  ip = s.getsockname()[0]
-  ip = "127.0.0.1" if ip == "0.0.0.0" else ip
-  s.close()
-  return ip
-
-IP = get_ip() + ":8080"
 
 #Printer code for raspy
 Epson = printer.Usb(0x04b8,0x0e28)
 Epson.text(sys.argv[1])
-Epson.qr('http://' + IP)
+Epson.qr('https://deruever.nl/', ec=0, size=3, model=2, native=False, center=True, impl='bitImageRaster')
 Epson.cut()
 
 print('Printing:' + sys.argv[1])
